67f9d87d7c527ea13c068174a0c43d0d0889a20b
tWhiteRabbit <radiantkicks@gmail.com>
2014-07-01 13:05:57 -0400

    First version of javascript calculations
    This is just a rough version of what will take place with the
    calculations. No visual updating yet.

--- js/calculator.js
+++ js/calculator.js
@@ -2,3 +2,52 @@
  * Javascript file for the functionality of the calculator
  * 
  */
+
+//	Global variable declaration
+var firstValue, secondValue, conjug = "0", secondPick = false, ;
+	
+/**
+ * On button press, grabs number and recalls it to memory to be used later
+ * @param  {string} value the number grabbed from the particular button pressed.
+ * @return {null}   returns no number, because it is waiting for either other numbers or a conjugative button press.
+ */
+function numberButtonPress(value) {
+	if (secondPick == false) {
+		firstValue = firstValue.concat(value);
+	} else {
+		secondValue = secondValue.concat(value);
+	}
+}
+
+/**
+ * Takes the value from the conjugative button, and starts a new numerical value.
+ * @param  {string} value a string with the conjugative mathematical term to be used: +, -, *, or /
+ * @return {null}   returns no value because it waits for the equals to finalize the equation
+ */
+function conjugButtonPress(value) {
+	conjug = value;
+	secondPick = true;
+}
+
+function equalButtonPress() {
+	var result;
+
+	switch (conjug) {
+		case "1":
+			result = firstValue + secondValue;
+			break;
+		case "2":
+			result = firstValue - secondValue;
+			break;
+		case "3":
+			result = firstValue / secondValue;
+			break;
+		case "4":
+			result = firstValue * secondValue;
+			break;
+		default:
+			break;
+	}
+
+	firstValue = 0, secondValue = 0, secondPick = false;
+}
